cf1e2deca9967f66d819ea226d47ccdabcb19c8a
sseezov <sseezov@mail.ru>
2023-12-04 11:56:56 +0700

    redone 1 step

---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    posts: 'Посты',
+    feeds: 'Фиды',
+    success: 'RSS успешно загружен',
+    button: 'Просмотр',
+    errors: {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      doubleRss: 'RSS уже существует',
+      emptyField: 'Поле не должно быть пустым',
+      noRSS: 'Ресурс не содержит валидный RSS',
+      NetworkError: 'Ошибка сети',
+    },
+  },
+};
--- src/validator.js
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-const schema = yup.string().matches(
-  /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)$/, 'введите правильный адрес')
-
-export default (str) => schema.validate(str);
